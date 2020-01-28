95291c5dbd15c2fa0ff0a40a575bc2cce014bd1c
weekend79 <mortenviken@yahoo.com>
2020-01-28 23:04:30 +0000

    added username in the gameboard

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,7 @@
 var score = 0; // The start score
 var answer;
 var username; 
-var questions = 10; // The total number of question in one game
+var totq = 10; // The total number of question in one game
 var qNo = 0;  // Question number 
 
 /*------------------------------------ Constants ----*/
@@ -11,8 +11,8 @@ const baseURL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=ea
 
 /*------------ Constants Stat boxes ------*/
 const usernameBox = localStorage.getItem(username)
-const correctAnswers = document.querySelector('correct-answers');
-//const score-box;
+const correctAnswers = document.querySelector('#correctAnswers');
+const scoreBox = document.querySelector('#scoreBox');
 
 /*------------ Constants Quiz Board ------*/
 const gameboard = document.querySelector('gameboard');
@@ -22,12 +22,16 @@ const answer1 = document.querySelector('answer1');
 const answer2 = document.querySelector('answer2');
 const answer3 = document.querySelector('answer3');
 const answer4 = document.querySelector('answer4');
+const game_bord_inner = [answer1, answer2, answer3, answer4];
 
 /*------------- Constant rewards --------*/
 const rewardEasy = 50;
 const rewardMed = 75;
 const rewardHard  = 100;
 /*------------Function---------*/
+// Code provided by my mentor Reuben Ferrante
+$('#usernameBox')[0].innerHTML += localStorage.getItem("username");
+// Code provided end!
 
 /*----- API Configuration -----
 function getData(category, cb) {
@@ -52,3 +56,13 @@ function writeToDocument() {
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+var questions = [
+    {'qtext': "What is your name?",
+     'game_bord_inner': [
+         'Reuben',
+         'Morten',
+         'Bob',
+         'Paul'
+     ],
+    'correct_answer': 0},
+]

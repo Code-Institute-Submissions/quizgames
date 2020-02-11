e05a0df1a021743ec4f01c07449fc037a3ce97fe
weekend79 <mortenviken@yahoo.com>
2020-02-11 17:22:21 +0000

    question and answers with clickon

--- js/quiz.js
+++ js/quiz.js
@@ -1,68 +1,147 @@
+console.log("Hello World!");
+
 /*----- Variables -----*/
 var score = 0; // The start score
-var answer;
-var username; 
-var totq = 10; // The total number of question in one game
-var qNo = 0;  // Question number 
+var username;
+var allQ = 10; // The total number of question in one game
+var qNum = 0;  // Question number 
+var qtext = document.querySelector('#qtext');
+
 
 /*------------------------------------ Constants ----*/
-/*----------Const API --------*/
-const baseURL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy"
-
 /*------------ Constants Stat boxes ------*/
 const usernameBox = localStorage.getItem(username)
 const correctAnswers = document.querySelector('#correctAnswers');
-const scoreBox = document.querySelector('#scoreBox');
+const scoreBox = document.querySelector('#scoreBox > h2');
 
 /*------------ Constants Quiz Board ------*/
 const gameboard = document.querySelector('gameboard');
-const qbox = document.querySelector('qbox');
-const qtext = document.querySelector('qtext');
-const answer1 = document.querySelector('answer1');
-const answer2 = document.querySelector('answer2');
-const answer3 = document.querySelector('answer3');
-const answer4 = document.querySelector('answer4');
-const game_bord_inner = [answer1, answer2, answer3, answer4];
+const qBox = document.querySelector('#qbox');
+const a = document.querySelector('#answer1');
+const b = document.querySelector('#answer2');
+const c = document.querySelector('#answer3');
+const d = document.querySelector('#answer4');
+const allAnswers = [a, b, c, d];
 
-/*------------- Constant rewards --------*/
-const rewardEasy = 50;
-const rewardMed = 75;
-const rewardHard  = 100;
+/*------------- Const rewards --------*/
+const reward = 100;
+
+/*-------------- Game Over -------------*/ 
+const gameOverSum = document.querySelector('#game-over-sum');
+const gameOverScore = document.querySelector('#game-over-score');
+
 /*------------Function---------*/
+
+/*--------------Update stats ---------*/
 // Code provided by my mentor Reuben Ferrante
 $('#usernameBox')[0].innerHTML += localStorage.getItem("username");
 // Code provided end!
 
-/*----- API Configuration -----
-function getData(category, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", baseURL + category);
-    xhr.send();
-
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-}
-
-function writeToDocument() {
-    getData(category, function(data) {
-        document.getElementById("data").innerHTML = data;
-    });
-}           */
-
 /*----- cached element references -----*/
 /*----- event listeners -----*/
-/*----- functions -----*/
-var questions = [
-    {'qtext': "What is your name?",
-     'game_bord_inner': [
-         'Reuben',
-         'Morten',
-         'Bob',
-         'Paul'
-     ],
-    'correct_answer': 0},
-]
+/*---------------------- Questions and Answers--------*/
+const allQuestions = [
+    {
+        questions: "What is your name?",
+        answers: {
+            a: "Reuben",
+            b: "Morten",
+            c: "Bob",
+            d: "Paul"
+        },
+        correct_answer: "a"
+    },
+    {
+        questions: "What is your Favourite dog?",
+        answers: {
+            a: "Spike",
+            b: "Doug",
+            c: "hjdsf",
+            d: "Charizard"
+        },
+        correct_answer: "b"
+    },
+    {
+        questions: "What is your name?",
+        answers: {
+            a: "Reuben",
+            b: "Morten",
+            c: "Bob",
+            d: "Paul"
+    },
+        correct_answer: "d"
+    }
+];
+
+function dqa() {
+    qtext = document.getElementById("qtext");
+    qtext.innerHTML = "<h3>" + allQuestions[qNum].questions + "</h3>"
+    document.getElementById("answer1").innerHTML = "<h3>" + allQuestions[qNum].answers["a"] + "</h3>"
+    document.getElementById("answer2").innerHTML = "<h3>" + allQuestions[qNum].answers["b"] + "</h3>"
+    document.getElementById("answer3").innerHTML = "<h3>" + allQuestions[qNum].answers["c"] + "</h3>"
+    document.getElementById("answer4").innerHTML = "<h3>" + allQuestions[qNum].answers["d"] + "</h3>"
+
+    console.log(qtext);
+  //  document.getElementById()
+
+}
+
+
+function checkAnswer(letter){
+    console.log(letter);
+    if(allQuestions[qNum].correct_answer == letter){
+        alert('You got it right!');
+        qNum++;
+        dqa();
+        return true;
+    } else {
+        qNum++;
+        dqa();
+        return false;
+    }
+}
+
+/*-- function userAnswer(callback) {
+    var letter = alert('You got it right!');
+    callback(letter);
+}
+
+userAnswer(); ---*/
+
+//     END OF TUTOR CALL ANSWERS
+
+function updateScore() {
+    if(checkAnswer(letter) === true) {
+        console.log("returned True")
+        score += Math.round(score + reward);
+        score++;
+    } else {
+        console.log("did not return True")
+    }
+}
+// updateScore();
+
+
+
+
+
+
+
+
+
+
+/*
+
+Variable for level.
+
+Display Questions -- Just need to build more on it
+
+Select Questions - ON CLICK FROM THE HTML
+
+Check if correct WE HAVE STARTED IT! :)
+    If Correct - Increase level
+
+
+
+
+*/

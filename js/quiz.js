6864fa92b7ae37e6a4d216da32f613f3a24397e6
weekend79 <mortenviken@yahoo.com>
2020-02-13 21:15:28 +0000

    score update

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,8 @@
-console.log("Hello World!");
+console.log("Hello hello World!");
 
 /*----- Variables -----*/
 var score = 0; // Start score
-var wins = 0;  // Start win
 var username;
-var allQ = 10; // The total number of question in one game
 var qNum = 0;  // Question number 
 var qtext = document.querySelector('#qtext');
 
@@ -12,8 +10,9 @@ var qtext = document.querySelector('#qtext');
 /*------------------------------------ Constants ----*/
 /*------------ Constants Stat boxes ------*/
 const usernameBox = localStorage.getItem(username)
-const winBox = document.querySelector('#winBox');
+const winBox = document.querySelector('#winBox > h3');
 const scoreBox = document.querySelector('#scoreBox > h3');
+const qNumBox = document.querySelector('#qNumBox > h3');
 
 /*------------ Constants Quiz Board ------*/
 const gameboard = document.querySelector('gameboard');
@@ -27,9 +26,7 @@ const allAnswers = [a, b, c, d];
 /*------------- Const rewards --------*/
 const reward = 100;
 
-/*-------------- Game Over -------------*/ 
-const gameOverSum = document.querySelector('#game-over-sum');
-const gameOverScore = document.querySelector('#game-over-score');
+
 
 /*------------Function---------*/
 
@@ -43,36 +40,108 @@ $('#usernameBox')[0].innerHTML += localStorage.getItem("username");
 /*---------------------- Questions and Answers--------*/
 const allQuestions = [
     {
-        questions: "What is your name?",
+        questions: "What is Javascript?",
         answers: {
-            a: "Reuben",
-            b: "Morten",
-            c: "Bob",
-            d: "Paul"
+            a: "A computer language",
+            b: "A planet in the galaxy",
+            c: "A drink",
+            d: "Who cares?"
         },
         correct_answer: "a"
     },
     {
-        questions: "What is your Favourite dog?",
+        questions: "What is the fullname of CSS?",
         answers: {
-            a: "Spike",
-            b: "Doug",
-            c: "hjdsf",
-            d: "Charizard"
+            a: "Canal Screen Seven",
+            b: "Cascading Style Sheet",
+            c: "Crazy seven sisters",
+            d: "Chess singel seas"
         },
         correct_answer: "b"
     },
     {
-        questions: "What is your name?",
+        questions: "What colours is it in the Norwegian flag?",
         answers: {
-            a: "Reuben",
-            b: "Morten",
-            c: "Bob",
-            d: "Paul"
+            a: "Green, Yellow and Red",
+            b: "Blue, Black and Red",
+            c: "Red, White and Blue",
+            d: "Yellow, Blue and Red"
     },
+        correct_answer: "c"
+    },
+    {
+        questions: "What is the name of the first know cryptocurrency?",
+        answers: {
+            a: "Ripple",
+            b: "Dash",
+            c: "Tron",
+            d: "Bitcoin"
+        },
+        correct_answer: "d"
+    },
+    {
+        questions: "What of the following names are a computerbrand?",
+        answers: {
+            a: "Axe",
+            b: "Zippy",
+            c: "Asus",
+            d: "IBG"
+        },
+        correct_answer: "c"
+    },
+    {
+        questions: "What is the best online software dev. school?",
+        answers: {
+            a: "Code Institue",
+            b: "Barkley",
+            c: "Code school",
+            d: "Institue school"
+        },
+        correct_answer: "a"
+    },
+    {
+        questions: "What is Coca Cola?",
+        answers: {
+            a: "A softdrink",
+            b: "A beer",
+            c: "A tea",
+            d: "A drink"
+        },
+        correct_answer: "a"
+    },
+    {
+        questions: "What language do they speak in Brazil?",
+        answers: {
+            a: "English",
+            b: "Python",
+            c: "Portugees",
+            d: "Spanish"
+        },
+        correct_answer: "c"
+    },
+    {
+        questions: "What team is Jurgen Klopp manager for?",
+        answers: {
+            a: "Brann",
+            b: "Liverpool",
+            c: "Vadmyra I.L",
+            d: "Dortmund"
+        },
+        correct_answer: "b"
+    },
+    {
+        questions: "Where do you put the page title in the HTML structure?",
+        answers: {
+            a: "Footer",
+            b: "No possible",
+            c: "Body",
+            d: "Head"
+        },
         correct_answer: "d"
     }
 ];
+
+/*--------- Display Questions and Answers ---------*/
 
 function dqa() {
     qtext = document.getElementById("qtext");
@@ -86,101 +155,51 @@ function dqa() {
   //  document.getElementById()
 
 }
-/*---------- Working code 
-function checkAnswer(letter){
-    console.log(letter);
-    if(allQuestions[qNum].correct_answer == letter){
-        qNum++;
-        dqa();
-    } else {
-        qNum++;
-        dqa();
-    }
-}
----------*/
 
 /*------- Check answer and update score ---------*/
-function updateScore(score) {
+
+function scoreBoard(score) {
     scoreBox.innerHTML = "<h3>" + score + "</h3>"
 }
 
 function checkAnswer(letter, callback){
+    console.log(letter);
     if(allQuestions[qNum].correct_answer == letter){
-        reward;
+        alert('You got it right!');
+        score += reward;
+        scoreBoard(score);
         qNum++;
         dqa();
     } else {
         qNum++;
         dqa();
     }
-    callback(score);
+    callback(letter);
+}
+// ---- Update score
+function updateScore(score, reward) {
+    console.log(score + reward);
 }
 
-checkAnswer()
-
-//     END OF TUTOR CALL ANSWERS
-
-/*----
-
-function updateScore(callback) {
-    var correctAnswer = score += Math.round(score + reward);
-    score++;
+// ----- Update Wins
+function updateWins() {
+    var wins = score / 100;
+    winBox.innerHTML = "<h3>" + wins + "</h3>"
 }
 
-correctAnswer(checkAnswer);
+updateWins(score/100);
 
+/*-------------- Game Over -------------*/ 
+const gameOverSum = document.querySelector('#game-over-sum');
+const gameOverScore = document.querySelector('#game-over-score');
 
---------*/
-
-/*-------- 
-
-function correctAnswer(callback) {
-    if(checkAnswer(letter) === true) {
-    score += Math.round(score + reward);
-    score++;
-    callback(score);
-    }
-    console.log(callback(score));
-    
-}
-
-// correctAnswer(myScore);
-
--------*/
-
-/*---   
-function updateScore(score) {
-    if(checkAnswer(letter) === true) {
-        console.log("returned True")
-        score += Math.round(score + reward);
-        score++;
+function endGame() {
+    if (allQuestions[qNum].questions ===  - 1) {
+        endGame();
     } else {
-        console.log("did not return True")
+        qNum++;
+        dqa();
     }
 }
-// updateScore();
 
 
-
-
-
-
-
-
-
-
-/*
-
-Variable for level.
-
-Display Questions -- Just need to build more on it
-
-Select Questions - ON CLICK FROM THE HTML
-
-Check if correct WE HAVE STARTED IT! :)
-    If Correct - Increase level
-
-
-
-
-*/
